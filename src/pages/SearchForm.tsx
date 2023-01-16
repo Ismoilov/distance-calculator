7ceb0c965ac02483dab124403627ab66300a1fac
Dilmurod <di.murod.1999@gmail.com>
2023-01-16 17:54:21 +0500

    searchForm changes

--- src/pages/SearchForm.tsx
+++ src/pages/SearchForm.tsx
@@ -1,18 +1,36 @@
-import { Row, Col, Form, Select } from 'antd';
+import { useState, useEffect } from 'react';
+import { Row, Col, Form, Select, Button } from 'antd';
 
 import Card from '../components/Card';
 import cities from '../cities';
 
+const { Option } = Select;
+const refactoredCities = cities.map(city => {
+  return {
+    label: city[0],
+    lat: city[1],
+    long: city[2],
+  }
+});
+
 const SearchForm = () => {
-  const refactoredCities = cities.map(city => {
-    return {
-      value: city[0],
-      label: city[0],
+  const [originCity, setOriginCity] = useState('');
+  const [originCityLoader, setOriginCityLoader] = useState(false);
+
+  useEffect(() => {
+    if (originCity !== '') {
+      setOriginCityLoader(true);
+      const getData = setTimeout(() => {
+        setOriginCityLoader(false);
+      }, 1000);
+      return () => clearTimeout(getData);
     }
-  });
-  
-  console.log(refactoredCities);
-  
+  }, [originCity])
+
+  const selectOriginCityHandler = (lat: string, data: any) => {
+    setOriginCity(data)
+  };
+
   const onFinish = (values: any) => {
     console.log('Success:', values);
   };
@@ -22,26 +40,48 @@ const SearchForm = () => {
       <Form
         layout='vertical'
         name="basic"
-        // labelCol={{ span: 8 }}
-        // wrapperCol={{ span: 16 }}
         onFinish={onFinish}
       >
         <Row>
           <Col md={24}>
             <Form.Item
-              label="Username"
-              name="username"
-              rules={[{ required: true, message: 'Please input your username!' }]}
+              label="Origin city"
+              name="originCity"
+              rules={[{ required: true, message: 'Please select your origin city!' }]}
             >
               <Select
                 showSearch
-                placeholder="Select a person"
+                loading={originCityLoader}
+                placeholder="Origin city"
                 optionFilterProp="children"
-                filterOption={(input, option) =>
-                  (option?.label.toString() ?? '').toLowerCase().includes(input.toLowerCase())
-                }
-                options={refactoredCities}
-              />
+                onSearch={val => setOriginCity(val)}
+                onSelect={selectOriginCityHandler}
+                getPopupContainer={trigger => trigger.parentNode}
+                filterOption={(input, option) => {
+                  return (option?.children?.toString() ?? '').toLowerCase().includes(input.toLowerCase())
+                }}
+              >
+                {refactoredCities.map(city => <Option key={city.lat} value={city.lat} long={city.long}>{city.label}</Option>)}
+              </Select>
+            </Form.Item>
+            <Form.Item
+              label="Intermediate cities"
+              name="interCities"
+              rules={[{ required: true, message: 'Please select your Intermediate cities!' }]}
+            >
+              <Select
+                mode="multiple"
+                allowClear
+                style={{ width: '100%' }}
+                placeholder="Please select"
+                getPopupContainer={trigger => trigger.parentNode}
+                // onChange={handleChange}
+              >
+                {refactoredCities.map(city => <Option key={city.lat} value={city.lat} long={city.long}>{city.label}</Option>)}
+              </Select>
+            </Form.Item>
+            <Form.Item>
+              <Button type='primary' htmlType='submit'>Submit</Button>
             </Form.Item>
           </Col>
         </Row>
