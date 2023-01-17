c2df0070d2c946fd385c5abfae3c6c18351c6e0d
Dilmurod <di.murod.1999@gmail.com>
2023-01-17 17:54:13 +0500

    some fixes

--- src/pages/SearchForm.tsx
+++ src/pages/SearchForm.tsx
@@ -14,11 +14,15 @@ const refactoredCities = cities.map(city => {
 });
 
 const SearchForm = () => {
-  const [originCity, setOriginCity] = useState('');
+  const [originCity, setOriginCity] = useState([]);
+  const [interCities, setInterCities] = useState([]);
+  const [destinationCity, setDestinationCity] = useState([]);
   const [originCityLoader, setOriginCityLoader] = useState(false);
+  const [interCitiesLoader, setInterCitiesLoader] = useState(false);
+  const [destinationCityLoader, setDestinationCityLoader] = useState(false);
 
   useEffect(() => {
-    if (originCity !== '') {
+    if (originCity.length !== 0) {
       setOriginCityLoader(true);
       const getData = setTimeout(() => {
         setOriginCityLoader(false);
@@ -27,12 +31,32 @@ const SearchForm = () => {
     }
   }, [originCity])
 
+  useEffect(() => {
+    if (interCities.length !== 0) {
+      setInterCitiesLoader(true);
+      const getData = setTimeout(() => {
+        setInterCitiesLoader(false);
+      }, 1000);
+      return () => clearTimeout(getData);
+    }
+  }, [interCities])
+
+  useEffect(() => {
+    if (destinationCity.length !== 0) {
+      setDestinationCityLoader(true);
+      const getData = setTimeout(() => {
+        setDestinationCityLoader(false);
+      }, 1000);
+      return () => clearTimeout(getData);
+    }
+  }, [destinationCity])
+
   const selectOriginCityHandler = (lat: string, data: any) => {
     setOriginCity(data)
   };
 
   const onFinish = (values: any) => {
-    console.log('Success:', values);
+    console.log(originCity, interCities);
   };
 
   return (
@@ -54,7 +78,6 @@ const SearchForm = () => {
                 loading={originCityLoader}
                 placeholder="Origin city"
                 optionFilterProp="children"
-                onSearch={val => setOriginCity(val)}
                 onSelect={selectOriginCityHandler}
                 getPopupContainer={trigger => trigger.parentNode}
                 filterOption={(input, option) => {
@@ -70,16 +93,38 @@ const SearchForm = () => {
               rules={[{ required: true, message: 'Please select your Intermediate cities!' }]}
             >
               <Select
+                loading={interCitiesLoader}
                 showSearch
                 mode="multiple"
                 allowClear
                 style={{ width: '100%' }}
                 placeholder="Please select"
                 getPopupContainer={trigger => trigger.parentNode}
+                onChange={(_, data: any) => {
+                  setInterCities(data)
+                }}
                 filterOption={(input, option) => {
                   return (option?.children?.toString() ?? '').toLowerCase().includes(input.toLowerCase())
                 }}
-              // onChange={handleChange}
+              >
+                {refactoredCities.map(city => <Option key={city.lat} value={city.lat} long={city.long}>{city.label}</Option>)}
+              </Select>
+            </Form.Item>
+            <Form.Item
+              label="Destination city"
+              name="destinationCity"
+              rules={[{ required: true, message: 'Please select your destination city!' }]}
+            >
+              <Select
+                showSearch
+                loading={destinationCityLoader}
+                placeholder="Destination city"
+                optionFilterProp="children"
+                onSelect={(_, data: any) => setDestinationCity(data)}
+                getPopupContainer={trigger => trigger.parentNode}
+                filterOption={(input, option) => {
+                  return (option?.children?.toString() ?? '').toLowerCase().includes(input.toLowerCase())
+                }}
               >
                 {refactoredCities.map(city => <Option key={city.lat} value={city.lat} long={city.long}>{city.label}</Option>)}
               </Select>
